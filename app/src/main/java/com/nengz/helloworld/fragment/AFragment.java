bdcc91aaa5658071f802fea1dfe7d80e80507c3f
ZhangNeng <neng.zhang@outlook.com>
2018-12-10 21:16:04 +0800

    fragment 3

--- app/src/main/java/com/nengz/helloworld/fragment/AFragment.java
+++ app/src/main/java/com/nengz/helloworld/fragment/AFragment.java
@@ -8,6 +8,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.nengz.helloworld.R;
@@ -15,6 +16,8 @@ import com.nengz.helloworld.R;
 public class AFragment extends Fragment {
 
     private TextView mTvTitle;
+    private Button mBtnChange,mBtnRest;
+    private BFragment bFragment;
 
     //传递参数
     public static AFragment newInstance(String title) {
@@ -36,18 +39,33 @@ public class AFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         mTvTitle = view.findViewById(R.id.tv_title);
+        mBtnChange = view.findViewById(R.id.btn_change);
+        mBtnRest = view.findViewById(R.id.btn_reset);
+        //转跳到BFragment
+        mBtnChange.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(bFragment == null) {
+                    bFragment = new BFragment();
+                }
+                Fragment fragment = getFragmentManager().findFragmentByTag("a");
+                if(fragment != null) {
+                    getFragmentManager().beginTransaction().hide(fragment).add(R.id.fl_container,bFragment).addToBackStack(null).commitAllowingStateLoss();     //返回上一个Fragment的时候，不刷新view
+                }else {
+                    getFragmentManager().beginTransaction().replace(R.id.fl_container, bFragment).addToBackStack(null).commitAllowingStateLoss();//addToBackStack返回时，返回到上一个Fragment而不是Activity replace是先销毁后创建view 因此之前view不会被保存
+                }
+            }
+        });
+        //改变文字
+        mBtnRest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mTvTitle.setText("我是新文字");
+            }
+        });
         if(getArguments() != null) {
             mTvTitle.setText(getArguments().getString("title"));
         }
     }
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-    }
 }
--- app/src/main/java/com/nengz/helloworld/fragment/ContainerActivity.java
+++ app/src/main/java/com/nengz/helloworld/fragment/ContainerActivity.java
@@ -11,27 +11,27 @@ import com.nengz.helloworld.R;
 public class ContainerActivity extends AppCompatActivity {
 
     private AFragment aFragment;
-    private BFragment bFragment;
-    private Button mBtnChange;
+//    private BFragment bFragment;
+//    private Button mBtnChange;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_container);
-        mBtnChange = findViewById(R.id.btn_change);
-        mBtnChange.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(bFragment == null) {
-                    bFragment = new BFragment();
-                }
-                getSupportFragmentManager().beginTransaction().replace(R.id.fl_container,bFragment).commitAllowingStateLoss();
-            }
-        });
+//        mBtnChange = findViewById(R.id.btn_change);
+//        mBtnChange.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if(bFragment == null) {
+//                    bFragment = new BFragment();
+//                }
+//                getSupportFragmentManager().beginTransaction().replace(R.id.fl_container,bFragment).commitAllowingStateLoss();
+//            }
+//        });
 
         //实例化AFragment
         aFragment = AFragment.newInstance("我是参数");
         //aFragment添加到Activity中,记得要用commit
-        getSupportFragmentManager().beginTransaction().add(R.id.fl_container,aFragment).commitAllowingStateLoss();
+        getSupportFragmentManager().beginTransaction().add(R.id.fl_container,aFragment,"a").commitAllowingStateLoss();  //给aFragment加了一个a的标识
     }
 }
